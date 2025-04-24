94d268453f25a743dcbcb376045ba208a15ec76b
Fidot02 <emekafelixabojei@gmail.com>
2025-04-24 15:32:46 +0100

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {  
+       static List<Person> persons = new List<Person>();
+
+        [HttpGet("GetMyName")]
+        public string GetMyName()
+        {
+            return "Felix";
+        }
+
+        [HttpGet("GetProductName")]
+        public string GetProductName(string product)
+        {
+            return $"The product name is {product}";
+        }
+        //Route Parameters technique
+        [HttpGet("MultipyNumbers/{num1}/{num2}")]
+        public int MultiplyNumbers([FromRoute] int num1,[FromRoute] int num2)
+        {
+            return num1 * num2;
+        }
+        //Query Parameters technique
+        [HttpGet("MultipyNumbers")]
+        public int MultiplyNumbers2( int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        //Body Parameters technique
+        //HTTP request contains a header and a body 
+        [HttpPost("SavePerson")]
+        public string SavePerson([FromBody]Person identity)
+        {
+            ProductsController.persons.Add(identity);
+            return "The Person has been successfully added.";
+        }
+        [HttpGet("ListOfPersons")]
+        public List<Person> GetAllPersons()
+        {
+            return ProductsController.persons;
+        }
+    } 
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Nationality { get; set; }
+        public string Gender { get; set; }
+
+        
+    }
+} 
+// REST APIs are a HTTP standard for creating APIs using the following verbs :
+//GET - Used to retrieve data 
+//POST - Used for creating data
+//PUT - Used for updating data 
+//DELETE -Used for removing data 
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//PART 1
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//PART 2
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//PART 3
+app.Run();
